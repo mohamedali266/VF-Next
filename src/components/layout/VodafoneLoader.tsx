cd0f9186f05a1987f954bcedb4f83f8ede20491a
mohamedali266 <orkidah9@gmail.com>
2026-09-20 12:20:31 +0300

    Improve Vodafone loader sweep animation

--- src/components/layout/VodafoneLoader.tsx
+++ src/components/layout/VodafoneLoader.tsx
@@ -14,16 +14,17 @@ export function VodafoneLoader({ fullscreen = false, label = "Loading" }: Vodafo
       <div className="vf-loader-mark">
         <svg className="vf-svg-loader" viewBox="0 0 500 500" width="190" height="190" aria-hidden="true">
           <g className="vf-loader-rings">
-            <path className="vf-loader-path vf-ring-1" d="M 82 355 A 214 214 0 0 1 390 84" fill="none" strokeWidth="7" strokeLinecap="round" />
-            <path className="vf-loader-path vf-ring-2" d="M 52 386 A 248 248 0 0 1 420 52" fill="none" strokeWidth="5" strokeLinecap="round" strokeDasharray="164 48" />
-            <path className="vf-loader-path vf-ring-3" d="M 87 420 A 276 276 0 0 0 456 170" fill="none" strokeWidth="5" strokeLinecap="round" strokeDasharray="128 54" />
-            <path className="vf-loader-path vf-sub-line-1" d="M 284 116 C 366 128 424 195 432 286" fill="none" strokeWidth="6" strokeLinecap="round" />
-            <path className="vf-loader-path vf-sub-line-2" d="M 302 154 C 364 171 405 223 408 294" fill="none" strokeWidth="5" strokeLinecap="round" strokeDasharray="92 38" />
+            <path className="vf-loader-path vf-ring vf-ring-outer" pathLength="1" d="M 306 42 C 167 39 54 149 54 287 C 54 401 139 461 258 459" fill="none" strokeWidth="5" strokeLinecap="round" />
+            <path className="vf-loader-path vf-ring vf-ring-mid" pathLength="1" d="M 330 68 C 210 52 86 143 82 279 C 78 390 158 435 266 431" fill="none" strokeWidth="7" strokeLinecap="round" />
+            <path className="vf-loader-path vf-ring vf-ring-inner" pathLength="1" d="M 354 105 C 260 82 125 151 122 281 C 120 367 188 399 278 392" fill="none" strokeWidth="9" strokeLinecap="round" />
+            <path className="vf-loader-path vf-ring vf-ring-right" pathLength="1" d="M 333 144 C 429 165 473 247 442 342 C 418 415 360 454 283 458" fill="none" strokeWidth="5" strokeLinecap="round" />
+            <path className="vf-loader-path vf-ring vf-ring-right-inner" pathLength="1" d="M 330 180 C 397 200 428 262 404 335 C 385 392 339 421 283 426" fill="none" strokeWidth="6" strokeLinecap="round" />
           </g>
 
           <g className="vf-loader-core">
-            <path className="vf-loader-path vf-core-main" d="M 317 105 C 240 82 143 126 109 238 C 66 378 170 451 292 428 C 386 410 435 319 405 235 C 389 191 351 161 300 151 C 286 148 282 128 317 105" fill="none" strokeWidth="22" strokeLinecap="round" strokeLinejoin="round" />
-            <path className="vf-loader-path vf-core-cut" d="M 312 154 C 350 166 378 194 391 233" fill="none" strokeWidth="12" strokeLinecap="round" />
+            <path className="vf-loader-path vf-core-main" d="M 317 84 C 235 79 150 139 124 240 C 92 363 180 434 296 405 C 386 382 424 287 382 219 C 362 187 326 166 283 162 C 280 129 292 101 317 84" fill="none" strokeWidth="17" strokeLinecap="round" strokeLinejoin="round" />
+            <path className="vf-loader-path vf-core-cut" d="M 302 152 C 345 158 376 181 392 216" fill="none" strokeWidth="8" strokeLinecap="round" />
+            <path className="vf-loader-path vf-core-top" d="M 292 96 C 330 92 363 102 389 122" fill="none" strokeWidth="7" strokeLinecap="round" />
           </g>
         </svg>
         <span className="vf-loader-label">{label}</span>
